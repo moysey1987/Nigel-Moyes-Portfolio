7313ae0cfc93be95985aa75c26cf3d59efdadf53
moysey1987 <nigelcmoyes@gmail.com>
2020-09-20 19:54:20 +1000

    Revert "New cursor"
    This reverts commit 47514045a7f0d520f1852a3074603ba50ddc4a0a.

--- js/cursor.js
+++
@@ -1,9 +0,0 @@
-const cursor = document.querySelector('.cursor');
-
-const editCursor = e => {
-    const { clientX: x, clientY: y } = e;
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-};
-
-window.addEventListener('mousemove', editCursor);
